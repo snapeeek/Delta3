c3b08865b6a6d477923d6e4c31be01f51724492d
snapeeek <maciekwozniak97@gmail.com>
2020-11-15 15:40:08 +0100

    Moved AngularJS routing htmls to partials folder

--- Delta3Mini/static/main.js
+++ Delta3Mini/static/main.js
@@ -3,11 +3,11 @@ var myapp = angular.module("app", ['ngRoute','angular-loading-bar']);
 myapp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
-            templateUrl : 'static/index.html',
+            templateUrl : 'static/partials/index.html',
             controller : 'IndexController'
         })
         .when('/login', {
-            templateUrl : 'static/login.html',
+            templateUrl : 'static/partials/login.html',
             controller : 'LoginController'
         })
     $locationProvider.hashPrefix('');
