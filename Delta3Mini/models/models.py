cbdb1cf7e43dd5818c3d06105b94e583b4c424c0
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-11-16 20:11:27 +0100

    database in progress

--- Delta3Mini/models/models.py
+++ Delta3Mini/models/models.py
@@ -9,6 +9,7 @@ def dump_datetime(value):
         return None
     return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -19,10 +20,29 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), default="Board")
+    background = db.Column(db.String(20))
+    labels = db.relationship('Label', backref='Board', lazy=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=True)
+    lists = db.relationship('List', backref='board', lazy=True)
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('Board.id'), nullable=False)
+    name = db.Column(db.String(50))
+    cards = db.relationship('Card', backref='card', lazy=True)
+
+
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    list_id = db.Column(db.Integer, db.ForeignKey('List.id'), nullable=False)
+    name = db.Column(db.String(50))
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    lists_of_elements = db.relationship('ListOfElements', backref='Card', lazy=True)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -36,3 +56,36 @@ class Card(db.Model):
             'date_created': dump_datetime(self.date_created),
         }
 
+
+class Label(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color = db.Column(db.String(30))
+    text = db.Column(db.String(50))
+
+
+class Element(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    done = db.Column(db.Boolean(False))
+
+
+class ListOfElements(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('Card.id'), nullable=False)
+
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    boards = db.relationship('Board', backref='team', lazy=True)
+    Name = db.Column(db.String(50))
+
+
+teams_and_users = db.Table('TeamsAndUsers',
+                           db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
+                           db.Column('team_id', db.Integer, db.ForeignKey('Team.id'), primary_key=True)
+                           )
+
+board_and_users = db.Table('BoardAndUsers',
+                           db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
+                           db.Column('Board_id', db.Integer, db.ForeignKey('Board.id'), primary_key=True)
+                           )
